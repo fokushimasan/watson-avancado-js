6265078aa16586315734d1b745e7ffdf125cd481
Welliton Klein <welliton.fokushima@gmail.com>
2019-04-08 17:40:21 -0300

    Tratando as respostas no servidor - parte 2

--- index.js
+++ index.js
@@ -1,58 +1,81 @@
-const port = 3000
+const port = 3000;
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const watson = require('watson-developer-cloud')
+const express = require('express');
+const bodyParser = require('body-parser');
+const watson = require('watson-developer-cloud');
 
-const watsonAuth = require('./watson-auth')
+const watsonAuth = require('./watson-auth');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-// variaveis de fonte desconhecida
+// variáveis de fonte desconhecida
+
 const sabores = {
     Calabreza: {
         valor: 15
     },
     Mussarela: {
-        valor: 12
+        valor: 10
     }
-}
-const tempo_de_entrega = '30 minutos'
+};
 
-const assistent = new watson.AssistantV1({
+const tempo_entrega = '30 minutos';
+
+const assistant = new watson.AssistantV1({
     username: watsonAuth.username,
     password: watsonAuth.password,
     url: 'https://gateway.watsonplatform.net/assistant/api/',
     version: '2018-02-16'
-})
+});
 
-app.get('/', (req, res) => {
-    return res.sendFile('./public/index.html')
-} )
+app.get('/', function (req, res) {
+    return res.sendFile('./public/index.html');
+});
 
 app.post('/dialog', (req, res) => {
-    const { message, context } = req.body
+    const { message, context } = req.body;
 
-    assistent.message(
+    assistant.message(
         {
             input: { text: message },
             workspace_id: watsonAuth.workspace_id,
             context
         },
-        function (error, response) {
-            if (error) {
-                console.error(error)
+        function (err, response) {
+            if (err) {
+                console.error(err);
             } else {
-                if (response.intents[0] && response.intents[0].intent == 'pedido') {
-                    console.log('Estamos recebendo um pedido!')
+                if (response.intents || response.intents[0].intent == 'pedido') {
+                    response.output.text.forEach((value, key) => {
+                        const { context } = response;
+
+                        if (context.quantidade && context.pizza) {
+
+                            const valor = context.quantidade * sabores[context.pizza].valor;
+                            const troco = context.troco ? `${context.troco - valor} de troco` : '';
+
+                            response.output.text[key] = value.replace(
+                                '{ valor }.',
+                                `${valor} reais ${troco}`
+                            );
+
+                            if ((context.troco) && (context.troco - valor < 0)) {
+                                response.output.text = ['Eu não posso mandar esse pedido! Você não tem dinheiro suficiente'];
+                            }
+                        }
+                    });
+
+                    response.output.text.forEach((value, key) => {
+                        response.output.text[key] = value.replace('{ tempo_entrega }', tempo_entrega);
+                    });
                 }
                 return res.json(response)
             }
         }
-    )
-})
+    );
+});
 
-app.listen(port, () => console.log(`Running on port ${port}`))
+app.listen(port, () => console.log(`Running on port ${port}`));
